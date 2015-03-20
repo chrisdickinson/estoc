7143e5efcc5778caddb87d641e872d127de6e927
Chris Dickinson <christopher.s.dickinson@gmail.com>
2015-03-19 22:23:43 -0700

    add support for checking exceptions

--- index.js
+++ index.js
@@ -1,231 +1,43 @@
 "use strict"
 
-module.exports = inspectCode
+module.exports = createUsageStream
 
-var makeTrackable = require('./lib/make-trackable.js')
-var defineRuntime = require('./lib/define-runtime.js')
-var escontrol = require('escontrol')
-var path = require('path')
+const visitPackage   = require('./lib/visit-package.js')
+const loadTARPackage = require('./lib/tar-package.js')
+const loadFSPackage  = require('./lib/fs-package.js')
+const Readable       = require('stream').Readable
+const path           = require('path')
+const fs             = require('fs')
 
-function inspectCode(baseCFG, cache, filename, pkg, ast, queue, ready) {
-  var maxIterations = 1e4
-  var cfg = escontrol(ast, {
-    onoperation: onoperation,
-    onfunction: onfunction,
-    oncalled: oncalled,
-    onload: onload,
-    oncall: oncall,
-    builtins: baseCFG ? baseCFG.builtins() : null,
-    global: baseCFG ? baseCFG.global() : null
-  })
-
-  if (!baseCFG) {
-    defineRuntime(cfg, trackUsage)
+function createUsageStream(filename, opts, ready) {
+  const stream = Readable({objectMode: true, read: noop})
+  switch (arguments.length) {
+    case 2:
+      ready = opts
+    case 1:
+      opts = {}
+      ready = typeof ready === 'function' ? ready : noop
   }
-  cfg.pause = pause
-  cfg.resume = resume
-  cfg.paused = 0
-  var gotFunction = null
-  var onFunctionMode = 0
+  fs.stat(filename, onstat)
+  return stream
 
-  function trackUsage(obj, name, kind) {
-    console.log('%s%s', pkg.name, name)
-  }
-
-  var requireFn = cfg.makeFunction(function(cfg, ctxt, args, isNew) {
-    var target = args[0]
-
-    if (!target.isString()) {
-      return cfg._valueStack.push(cfg.makeUnknown())
+  function onstat(err, stat) {
+    if (err) {
+      return stream.emit('error', err), ready(err)
     }
+    var loadPackage = stat.isDirectory() ?
+      loadFSPackage :
+      loadTARPackage
+    loadPackage(filename, onpkg)
+  }
 
-    var val = target._value
-    if (!val) {
-      return cfg._valueStack.push(cfg.makeUnknown())
+  function onpkg(err, pkg) {
+    if (err) {
+      return stream.emit('error', err), ready(err)
     }
-
-    //if (pkg.hasDependency(val.split(/\/\\/g)[0])) {
-    //  return cfg._valueStack.push(cfg.makeUnknown())
-    //}
-
-    if (!/^(\w:\\|\\|\/|\.)/.test(val)) {
-      return cfg._valueStack.push(makeTrackable(cfg, filename + ': ' + val, trackUsage))
-    }
-
-    if (global.cnt == 29) debugger
-    cfg.pause()
-    pkg.resolveFilename(filename, val, function(err, targetFile) {
-      if (err) {
-        return ready(err)
-      }
-
-      if (cache.has(targetFile)) {
-        cfg.resume()
-        cfg._valueStack.push(cache.get(targetFile))
-        return iterate()
-      }
-
-      if (path.extname(targetFile) === '.json') {
-        cfg.resume()
-        cache.set(targetFile, cfg.makeObject())
-        cfg._valueStack.push(cache.get(targetFile))
-        return iterate()
-      }
-
-      pkg.getASTFor(targetFile, function(err, ast) {
-        if (err) {
-          return ready(err)
-        }
-        cache.set(targetFile, cfg.makeObject())
-        inspectCode(cfg, cache, targetFile, pkg, ast, queue, onready)
-      })
-
-      function onready(err, value) {
-        if (err) {
-          return ready(err)
-        }
-
-        cache.set(targetFile, value)
-        cfg._valueStack.push(value)
-        cfg.resume()
-        setImmediate(iterate)
-      }
-    })
-  })
-
-  var moduleObj = cfg.makeObject()
-  var exportsObj = cfg.makeObject()
-  var filenameVal = cfg.makeValue('string', filename)
-  var dirnameVal = cfg.makeValue('string', path.dirname(filename))
-  var functions = []
-  moduleObj.newprop('exports').assign(exportsObj)
-  cache.set(filename, exportsObj)
-
-  while(cfg.advance()) {
-     /* noop */
-  }
-
-  if (!gotFunction) {
-    return ready(new Error('could not find module wrapper!'))
-  }
-
-  gotFunction.call(cfg, cfg.global(), [
-    exportsObj,
-    requireFn,
-    moduleObj,
-    filenameVal,
-    dirnameVal
-  ])
-  setImmediate(iterate)
-
-  function iterate() {
-    var i = 0
-    while (!cfg.paused && cfg.advance()) {
-      if (++i > 1000) {
-        setImmediate(iterate)
-        return
-      }
-    }
-    if (cfg.paused) return
-    oncomplete()
-  }
-
-  function oncomplete() {
-    cfg._edges.length = 0
-    var cb = ready
-    ready = noop
-    if (functions.length) {
-      queue.push(function(cb) {
-        ready = cb
-        var len = functions.length
-        functions = functions.filter(function(xs) {
-          return xs.sharedFunctionInfo().callCount() === 0
-        })
-        setImmediate(iter)
-      })
-    }
-
-    // this gets into weird territory.
-    // cb(null, cfg.makeUnknown())
-    cb(null, moduleObj.getprop('exports').value())
-
-    function iter() {
-      if (!functions.length) {
-        var cb = ready
-        ready = noop
-        return cb(null)
-      }
-      var next = functions.shift()
-      var sfi = next.sharedFunctionInfo()
-      if (sfi.callCount()) {
-        return iter(ready)
-      }
-
-      var argLen = next._code.params.length
-      var args = []
-
-      var marks = next.getMark('ioc')
-
-      for(var i = 0; i < argLen; ++i) {
-        args.push(marks.length ?
-          makeTrackable(cfg, marks[0].concat(['<ioc-arg #' + i + '>']), trackUsage) :
-          cfg.makeUnknown()
-        )
-      }
-
-      next.call(cfg, cfg.makeUnknown(), args)
-      try {
-        iterate()
-      } catch(err) {
-        oncomplete()
-      }
-    }
-  }
-
-  function onoperation(args, op, result) {
-
-  }
-
-  function oncall(fn, context, args, recursion) {
-    if (fn.sharedFunctionInfo) {
-      var sfi = fn.sharedFunctionInfo()
-      return sfi._records < 1
-    }
-  }
-
-  function oncalled(fn, context, args, recursion, result) {
-  }
-
-  function onload(name, value) {
-    if (value.__title__) {
-      trackUsage(null, value.__title__.join('.') + ' (as ' + name.getName() + ')')
-    }
-  }
-
-  function onfunction(fn, astNode) {
-    switch (onFunctionMode) {
-      case 0:
-        gotFunction = fn
-        onFunctionMode = 1
-      break;
-      case 1:
-        functions.push(fn)
-        // hah, okay. cool story, really.
-      break;
-    }
-  }
-
-  function pause() {
-    if (baseCFG && baseCFG.pause) baseCFG.pause()
-    ++cfg.paused
-  }
-
-  function resume() {
-    if (baseCFG && baseCFG.resume) baseCFG.resume()
-    --cfg.paused
+    visitPackage(pkg, stream, opts, ready)
   }
 }
 
-function noop() {
-
+function noop(n) {
 }
--- test.js
+++ test.js
@@ -1,40 +1,13 @@
-var FSPackage = require('./lib/fs-package.js')
-var path = require('path')
+const createUsageStream = require('./index.js')
 
-var cache = new Map
-var pkg = new FSPackage(path.join(path.resolve(process.argv[2]), 'package.json'))
+var stream    = require('stream')
+var Transform = stream.Transform
 
-var inspect = require('./index.js')
-
-pkg.getEntryPoints(function(err, entryPoints) {
-  console.log(entryPoints)
-  var queue = []
-  iter()
-
-  function iter() {
-    if(!entryPoints.length) return iterQueue()
-    
-    var entry = entryPoints.shift()
-    pkg.getASTFor(entry, function(err, ast) {
-      if (err) throw err
-      inspect(null, cache, entry, pkg, ast, queue, function(err, value) {
-        iter()
-      })
-    })
-  }
-
-  function iterQueue() {
-    if (!queue.length) {
-      return
-    }
-    var next = queue.shift()
-    next(iterQueue)
+var txf = new Transform({
+  objectMode: true,
+  transform: function(usage, _, ready) {
+    ready(null, new Buffer(String(usage.info() || '') + '\n'))
   }
 })
 
-if(false)
-pkg.getASTFor('/test.js', function(err, ast) {
-  inspect(null, cache, '/test.js', pkg, ast, function(err, value) {
-    
-  })
-})
+createUsageStream(process.argv[2]).pipe(txf).pipe(process.stdout)
